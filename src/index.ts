92d43dd3c940d8586fb31b1f3c3de0af80f087f8
Edwin Capel <edwinrc1026@gmail.com>
2023-10-31 01:58:08 +0800

    initialise bun app with get all routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-import { Elysia } from "elysia";
+import { Elysia } from "elysia"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const data = [{
+  id: 1,
+  title: 'First Item',
+  description: "Description for the first item",
+  isComplete: false
+},
+{
+  id: 2,
+  title: 'Second Item',
+  description: "Description for the second item",
+  isComplete: false
+},
+{
+  id: 3,
+  title: 'Third Item',
+  description: "Description for the third item",
+  isComplete: false
+}]
+
+const app = new Elysia()
+.get("/all", ()=> {
+  return { data }
+})
+.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
